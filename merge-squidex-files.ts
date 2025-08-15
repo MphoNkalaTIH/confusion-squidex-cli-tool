b6b19e2a991248be61a89765c76d2e5728072e19
Mpho Nkala <mphon@tihsa.co.za>
2025-08-15 13:20:56 +0200

    uupdated merge to only target = the data part of the qoute section

--- merge-squidex-files.ts
+++ merge-squidex-files.ts
@@ -8,15 +8,23 @@ const file2 = process.argv[3]
 const content1 = await fs.readFile(file1, 'utf8');
 const content2 = await fs.readFile(file2, 'utf8');
 
+const content1Json = JSON.parse(content1);
+const content2Json = JSON.parse(content2);
+
 //create a patch (unified diff format)
-const patch = createTwoFilesPatch(file2, file1, content2, content1);
+const patch = createTwoFilesPatch(file2, file1, JSON.stringify(content2Json.data), JSON.stringify(content1Json.data));
 
 //apply the patch to content2 (file2), producing a merged version matching file1
-const merged = applyPatch(content2, patch);
+const merged = applyPatch(JSON.stringify(content2Json.data), patch);
+
+
 
 //write the merged result back to file2
 if(merged){
-    await fs.writeFile(file2, merged, 'utf8');
+    content2Json.data = JSON.parse(merged);
+    
+    await fs.writeFile(file2, JSON.stringify(content2Json, null, 2), 'utf8');
+
     console.log(`Merged diff from ${file1} into ${file2}, updating ${file2} to match ${file1}`);
 }
 else{
