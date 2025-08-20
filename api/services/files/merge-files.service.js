478f7822689690cb967fd6c6c7f205a05b7ac697
Mpho Nkala <mphon@tihsa.co.za>
2025-08-20 12:25:04 +0200

    updated backend services for files and squidex

---
+++ api/services/files/merge-files.service.js
@@ -0,0 +1,36 @@
+import fs from 'fs/promises';
+import {applyPatch, createTwoFilesPatch } from 'diff';
+
+export async function mergeSquidexContentFiles(mergeFromFilePath, mergeIntoFilePath) {
+
+    //read both files as strings
+    const content1 = await fs.readFile(mergeFromFilePath, 'utf8');
+    const content2 = await fs.readFile(mergeIntoFilePath, 'utf8');
+
+    // we need to parse the JSON content to access the data field
+    const content1Json = JSON.parse(content1);
+    const content2Json = JSON.parse(content2);
+
+    //create a patch (unified diff format)
+    const patch = createTwoFilesPatch(mergeIntoFilePath, mergeFromFilePath, JSON.stringify(content2Json.data), JSON.stringify(content1Json.data));
+
+    //apply the patch to content2 (mergeIntoFilePath), producing a merged version matching mergeFromFilePath
+    const merged = applyPatch(JSON.stringify(content2Json.data), patch);
+
+    //write the merged result back to file2
+    if(merged){
+        content2Json.data = JSON.parse(merged);
+        
+        await fs.writeFile(mergeIntoFilePath, JSON.stringify(content2Json, null, 2), 'utf8');
+
+        console.log(`Merged diff from ${mergeFromFilePath} into ${mergeIntoFilePath}, updating ${mergeIntoFilePath} to match ${mergeIntoFilePath}`);
+    }
+    else{
+        console.error(`Failed to merge ${mergeFromFilePath} into ${mergeIntoFilePath}. The files may not be compatible.`);
+        //TODO: need to implement error handling on the web page
+    }
+
+}
+
+
+
---
+++ api/services/squidex/fetch-content.service.js
@@ -0,0 +1,137 @@
+import axios from 'axios';
+import  'dotenv/config';
+import fs from 'fs/promises';
+import path from 'path';
+
+function getOutputDir(schema) {
+  switch(schema) {
+    case "qoute-sections":
+      return 'output';
+
+    case "qoute-flow":
+      return 'qoute-flow';
+
+    case "static-data":
+      return 'static-data';
+
+    case "enums":
+      return 'static-enums';
+    
+    default:
+      console.error(`[#] Unknown schema: ${schema} [#]`);
+      return '';
+  }
+}
+
+function getEnvironmentUrl(app, schema) {
+  const key = `${app} ${schema}`;
+
+  switch(key) {
+    case "con-fusion qoute-sections":
+      return 'DEV_QOUTE_SECTIONS_URL';
+
+    case "con-fusion qoute-flow":
+      return 'DEV_QOUTE_FLOW_URL';
+
+    case "con-fusion-static static-data":
+      return 'DEV_STATIC_DATA_URL';
+
+    case "con-fusion-static enums":
+      return 'DEV_ENUMS_URL';
+    default:
+      return '';
+  }
+}
+
+async function getComponentsData(token, app, schema) {
+  const ENVIRONMENT_URL = getEnvironmentUrl(app, schema);
+
+  return await axios.get<any>(
+    process.env[ENVIRONMENT_URL] ?? '',
+    {
+      headers: 
+      { 
+        Authorization: `Bearer ${token}`,
+      }
+    }
+  ).then((resp) =>{
+    console.log("[#] Done doing fetch components from squidex [#]");
+    return resp.data.items;
+  }).catch((err)=> {
+    console.log({error_fetching: err})
+    return null
+  })
+}
+
+function getNameAsRoute(name){
+  return name.replace(" ", "-").toLocaleLowerCase();
+}
+
+function buildFilePath(app , data, id , outDir ) {
+  if(app === "con-fusion-static") {
+    return path.join(outDir, `${id}.json`);
+  }
+
+  else if(app === "con-fusion") {
+    let env = data.environmentId?.iv ?? '';
+    let brand = data.brand?.iv ?? '';
+    let route = data.route?.iv ?? '';
+    let baseRoute = data.baseRoute?.iv != "" ? data.baseRoute.iv :  getNameAsRoute(data.name.iv);
+
+    const folderPath = path.join(outDir, env, brand, baseRoute);
+
+    let fileName = '';
+    if(route == "") fileName = `${baseRoute}-${id}.json`;
+    else fileName = `${route}-${id}.json`;
+    
+    return path.join(folderPath, fileName);
+  }
+
+  else return '';
+}
+
+async function writeDataFiles(app, items, outDir) {
+  for (const item of items) {
+    const filePath = buildFilePath(app, item.data, item.id, outDir);
+    await fs.mkdir(path.dirname(filePath), { recursive: true });
+
+    try {
+      await fs.access(filePath);
+      console.log(`Updating existing file: ${filePath}`);
+    } catch {
+      console.log(`Creating new file: ${filePath}`);
+    }
+
+    await fs.writeFile(filePath, JSON.stringify(item, null, 2), 'utf-8');
+  }
+}
+
+export async function fetchSquidexContent(PULL_FROM_APP, PULL_FROM_SCHEMA) {
+  try {
+    const ACCESS_TOKEN = process.env.TOKEN
+    if(!ACCESS_TOKEN){
+      console.error('[#] No access token found. Please set the TOKEN environment variable [#]');
+      return
+    }; 
+
+    if(!PULL_FROM_APP || !PULL_FROM_SCHEMA) {
+      console.error('[#] Usage: node pull-latest-squidex.js <pull-from-app> <pull-from-schema> [#]');
+      return;
+    }
+
+    console.log(`Pulling latest data from Squidex app: ${PULL_FROM_APP}, schema: ${PULL_FROM_SCHEMA}`);
+
+    const items = await getComponentsData(ACCESS_TOKEN, PULL_FROM_APP, PULL_FROM_SCHEMA);
+    if(!items) {
+      console.error('[#] No items found or error fetching data [#]');
+      return;
+    }
+    
+    const outputDir = getOutputDir(PULL_FROM_SCHEMA);
+    if(items) await writeDataFiles(PULL_FROM_APP, items, `./${outputDir}`);
+
+    console.log(`Data files written to ./${outputDir}`);
+  } catch (err) {
+    console.error('Error fetching Squidex data:', err);
+  }
+}
