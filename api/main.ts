ba339f6b158eb4a7c1e199b57c254c0de5aafc7d
Mpho Nkala <mphon@tihsa.co.za>
2025-09-03 13:19:40 +0200

    a bit of a cleanup and current setups

--- api/main.ts
+++ api/main.ts
@@ -1,23 +1,47 @@
 import express, { type Request, type Response } from 'express';
-import { fetchSquidexContent } from "./services/squidex/fetch-content.service.ts";
+import { getFolderContent, getRoot } from './services/files/get-all-files.service.ts';
+import { fetchSquidexContent } from './services/squidex/fetch-content.service.ts';
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// files routes
-app.get("/files/get-all-files", ( req: Request, res: Response) =>  {res.status(200).send("OK")});
-app.get("/files/get-specific-file", ( req: Request, res: Response) => {res.status(200).send("OK")});
-app.put("/files/merge-files", (req: Request, res: Response) => {res.status(200).send("OK")});
+// files and folders routes
+app.get('/files/get-root', async (req: Request, res: Response) => {
+  const files = await getRoot();
+  res.status(200).send(files);
+});
+app.get('/files/get-folder-content', async (req: Request, res: Response) => {
+  const params = req.query;
+
+  const parent = typeof params.parent === 'string' ? params.parent : undefined;
+  const self = typeof params.self === 'string' ? params.self : undefined;
+
+  console.log({ parent, self });
+
+  const files = await getFolderContent(parent, self);
+
+  res.status(200).send(files);
+});
+app.get('/files/get-specific-file', (req: Request, res: Response) => {
+  res.status(200).send('OK');
+});
+app.put('/files/merge-files', (req: Request, res: Response) => {
+  res.status(200).send('OK');
+});
 
 // squidex routes
-app.delete("/squidex/delete-content", (req: Request, res: Response) => {res.status(200).send("OK")});
-app.get("/squidex/fetch-content", async (req: Request, res: Response) => {
+app.delete('/squidex/delete-content', (req: Request, res: Response) => {
+  res.status(200).send('OK');
+});
+app.get('/squidex/fetch-content', async (req: Request, res: Response) => {
   const params = req.query;
   await fetchSquidexContent(params.app, params.schema);
-  res.status(200).send("OK");
+  res.status(200).send('OK');
 });
-app.put("/squidex/update-content", (req: Request, res: Response) => {res.status (200).send("OK")});
+app.put('/squidex/update-content', (req: Request, res: Response) => {
+  res.status(200).send('OK');
+});
 
 app.listen(PORT, () => {
-  console.log("Server Listening on PORT:", PORT);
+  console.log('Server Listening on PORT:', PORT);
 });
---
+++ api/services/files/get-all-files.service.ts
@@ -0,0 +1,62 @@
+import fs from 'fs/promises';
+
+export async function getRoot() {
+  const files = await fs.readdir('squidex');
+  const mappedFiles = files?.map((file) => ({ parent: 'squidex', self: file }));
+  const formattedFiles = await getFileOrFolderDetails(mappedFiles);
+
+  return formattedFiles;
+}
+
+export async function getFolderContent(parent?: string, self?: string) {
+  const files = await fs.readdir(`${parent}/${self}`);
+  const mappedFiles = files?.map((file) => ({ parent: `${parent}/${self}`, self: file }));
+  const formattedFiles = await getFileOrFolderDetails(mappedFiles);
+
+  return formattedFiles;
+}
+
+function getFileOrFolderDetails(files: { parent: string; self: string }[]) {
+  return Promise.all(
+    files.map(async (file) => {
+      const filePath = `${file.parent}/${file.self}`;
+
+      const stat = await fs.stat(filePath);
+
+      const type = stat.isDirectory() ? 'directory' : 'file';
+      const size = formatSize(stat.size);
+      const modified = stat.mtime;
+      const created = stat.birthtime;
+
+      const identifier =
+        type == 'directory'
+          ? filePath
+          : await fs.readFile(filePath, 'utf-8').then((content) => JSON.parse(content).id);
+
+      return {
+        id: identifier,
+        label: file.self,
+        type,
+        size,
+        modified,
+        created,
+        relativeFilePath: filePath,
+        marked: false,
+      };
+    })
+  );
+}
+
+function formatSize(bytes: number): string {
+  if (bytes < 1024) return `${bytes} B`;
+  if (bytes < 1024 * 1024) return `${(bytes / 1024).toFixed(2)} KB`;
+  if (bytes < 1024 * 1024 * 1024) return `${(bytes / (1024 * 1024)).toFixed(2)} MB`;
+  return `${(bytes / (1024 * 1024 * 1024)).toFixed(2)} GB`;
+}
+
+function isValidPath(path: string) {
+  return fs
+    .stat(path)
+    .then((stat) => stat.isDirectory() || stat.isFile())
+    .catch(() => false);
+}
