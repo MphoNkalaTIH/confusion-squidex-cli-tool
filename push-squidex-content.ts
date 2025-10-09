4249a1c888aa90207f9a05b786e11a65abf0e53a
Mpho Nkala <mphon@tihsa.co.za>
2025-10-09 10:50:29 +0200

    updated squidex sit

--- push-squidex-content.ts
+++ push-squidex-content.ts
@@ -2,42 +2,53 @@ import axios from "axios";
 import fs from 'fs/promises';
 import 'dotenv/config';
 
-async function pushUpdatedContent( file: string): Promise<void | null> {  
+async function pushUpdatedContent(file: string): Promise<void | null> {  
+  const environment = process.env.ENVIRONMENT;
+  const ACCESS_TOKEN = process.env[`${environment}_TOKEN`];
+
+  if (!ACCESS_TOKEN) {
+    console.error('[#] No access token found. Please set the TOKEN environment variable [#]');
+    return;
+  }
+
   const fileContent = await fs.readFile(file, 'utf8');
-  
   const fileContentJson = JSON.parse(fileContent);
   const fileContentData = fileContentJson.data;
-
   const PUSH_URL = fileContentJson._links.update.href;
+  const BASE_URL = process.env[`${environment}_BASE_URL`];
+
+  if (!BASE_URL) {
+    console.error(`[#] No base URL found for environment: ${environment} [#]`);
+    return;
+  }
 
   return await axios.put<any>(
-    `${process.env.DEV_BASE_URL}${PUSH_URL}`,
+    `${BASE_URL}${PUSH_URL}`,
     fileContentData,
     {
-      headers: 
-      { 
-        Authorization: `Bearer ${process.env.DEV_TOKEN}`,
+      headers: { 
+        Authorization: `Bearer ${ACCESS_TOKEN}`,
       }
     }
-  ).then((resp) =>{
-    console.log(`[#] Done updating squidex content [#]`);
-
+  ).then((resp) => {
+    console.log(`[#] Done updating squidex content for ${environment} environment [#]`);
     return console.log({put_content_response: resp});
-  }).catch((err)=> {
-    console.log({error_fetching: err.response});
-    return null
-  })
+  }).catch((err) => {
+    console.log({error_pushing: err.response});
+    return null;
+  });
 }
 
 (async () => {
   try {
-    const file =  process.argv[2];
+    const file = process.argv[2];
 
     if (!file) {
       console.error("Usage: node push-squidex-data.js <file>");
       return;
     }
     
+    console.log(`Pushing content to ${process.env.ENVIRONMENT} environment`);
     await pushUpdatedContent(file);
   } catch (err) {
     console.error('Error updating Squidex content with error:', err);
